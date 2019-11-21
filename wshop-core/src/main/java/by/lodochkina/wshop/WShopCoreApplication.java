b1f54f3c299de573f8c0d10b63320a81bfb2ae41
Sergei Ivko <srgivko@gmail.com>
2019-11-21 19:13:14 +0300

    Initial Code Setup

---
+++ wshop-core/src/main/java/by/lodochkina/wshop/WShopCoreApplication.java
@@ -0,0 +1,11 @@
+package by.lodochkina.wshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WShopCoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WShopCoreApplication.class, args);
+    }
+}
