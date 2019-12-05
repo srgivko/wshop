4d88f870fdeda7ea81edf6b841955a8fe7e8bd89
Sergei Ivko <srgivko@gmail.com>
2019-12-05 16:49:38 +0300

    resources bundle in admin site (change the template views)

--- wshop-admin/src/main/java/by/lodochkina/wshop/admin/configs/WebConfig.java
+++ wshop-admin/src/main/java/by/lodochkina/wshop/admin/configs/WebConfig.java
@@ -20,13 +20,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 import javax.servlet.Filter;
 import javax.servlet.MultipartConfigElement;
+import java.util.Locale;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -125,4 +130,22 @@ public class WebConfig implements WebMvcConfigurer {
         return bean;
     }
 
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.forLanguageTag("ru"));
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
 }
--- wshop-admin/src/main/java/by/lodochkina/wshop/admin/configs/WebSecurityConfig.java
+++ wshop-admin/src/main/java/by/lodochkina/wshop/admin/configs/WebSecurityConfig.java
@@ -50,7 +50,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/dashboard")
+                .successForwardUrl("/dashboard")
                 .failureUrl("/login?error")
                 .permitAll()
                 .and()
